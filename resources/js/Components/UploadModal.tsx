71a258b879e097d00bf2c1c0777455f66704dd25
laupwing <laupwing@gmail.com>
2024-08-02 17:04:34 +0200

    Refactor Welcome page layout to use UploadModal component

--- resources/js/Components/UploadModal.tsx
+++ resources/js/Components/UploadModal.tsx
@@ -3,6 +3,7 @@ import { Button } from "./ui/button"
 import { Camera, ImageUp } from "lucide-react"
 import { Label } from "./ui/label"
 import { Textarea } from "./ui/textarea"
+import { Input } from "./ui/input"
 
 export const UploadModal = () => {
     return (
@@ -20,13 +21,18 @@ export const UploadModal = () => {
                 </Button>
                 <span className="text-slate-200 my-2">OR</span>
                 <Button
-                    className="uppercase w-full gap-2 h-12 flex items-center justify-center text-slate-400"
+                    className="uppercase w-full  h-12 flex items-center justify-center text-slate-400"
                     variant={"outline"}
                 >
-                    <ImageUp />
-                    Upload Photo
+                    <Label
+                        htmlFor="picture"
+                        className="w-full flex items-center justify-center gap-2"
+                    >
+                        <ImageUp />
+                        Upload Photo
+                    </Label>
                 </Button>
-                {/* <Input id="picture" type="file" /> */}
+                <Input className="hidden" id="picture" type="file" />
                 <div className="grid gap-2 w-full mt-4">
                     <Label htmlFor="description">Description</Label>
                     <Textarea
