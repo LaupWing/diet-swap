5644c88c9794ce60342170e7e4677b402732e6dd
laupwing <laupwing@gmail.com>
2024-08-02 18:03:19 +0200

    Refactor MealModal component to handle file input change event

--- resources/js/Components/MealModal.tsx
+++ resources/js/Components/MealModal.tsx
@@ -4,10 +4,12 @@ import { Camera, ImageUp } from "lucide-react"
 import { Label } from "./ui/label"
 import { Textarea } from "./ui/textarea"
 import { Input } from "./ui/input"
+import { useState } from "react"
 
 export const MealModal = () => {
+    const [preview, setPreview] = useState<string | null>(null)
     const handleChange = (e: any) => {
-        console.log(e.target!.files)
+        setPreview(URL.createObjectURL(e.target!.files[0]))
     }
 
     return (
@@ -16,26 +18,28 @@ export const MealModal = () => {
                 <DialogTitle>Add Meal</DialogTitle>
             </DialogHeader>
             <div className="flex items-center flex-col">
-                <Button
-                    className="uppercase w-full gap-2 h-12 flex items-center justify-center text-slate-400"
-                    variant={"outline"}
-                >
-                    <Camera />
-                    Make Photo
-                </Button>
-                <span className="text-slate-200 my-2">OR</span>
-                <Button
-                    className="uppercase w-full  h-12 flex items-center justify-center text-slate-400"
-                    variant={"outline"}
-                >
-                    <Label
-                        htmlFor="picture"
-                        className="w-full flex items-center justify-center gap-2"
+                <div className="flex items-center w-full flex-col">
+                    <Button
+                        className="uppercase w-full gap-2 h-12 flex items-center justify-center text-slate-400"
+                        variant={"outline"}
                     >
-                        <ImageUp />
-                        Upload Photo
-                    </Label>
-                </Button>
+                        <Camera />
+                        Make Photo
+                    </Button>
+                    <span className="text-slate-200 my-2">OR</span>
+                    <Button
+                        className="uppercase w-full  h-12 flex items-center justify-center text-slate-400"
+                        variant={"outline"}
+                    >
+                        <Label
+                            htmlFor="picture"
+                            className="w-full flex items-center justify-center gap-2"
+                        >
+                            <ImageUp />
+                            Upload Photo
+                        </Label>
+                    </Button>
+                </div>
                 <Input
                     onChange={handleChange}
                     className="hidden"
