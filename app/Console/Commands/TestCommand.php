6cadae10fc122527271b70e20625bac04e7a1368
laupwing <laupwing@gmail.com>
2024-08-02 16:25:30 +0200

    feat: Integrate OpenAI chat API in TestCommand

--- app/Console/Commands/TestCommand.php
+++ app/Console/Commands/TestCommand.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use OpenAI;
 
 class TestCommand extends Command
 {
@@ -25,6 +26,37 @@ class TestCommand extends Command
      */
     public function handle()
     {
-        //
+        $open_ai = OpenAI::client(env("OPENAI_API_KEY"));
+        $content =  "";
+        $response = $open_ai->chat()->create([
+            "model" => "gpt-3.5-turbo-1106",
+            "response_format" => [
+                "type" => "json_object",
+            ],
+            "messages" => [
+                [
+                    "role" => "system",
+                    "content" => "You are a helpful assistant designed to generate recipes and output in JSON format. Each recipe should include the properties 'name', 'description', 'nutrients', 'ingredients', 'cooktime_in_minutes', and 'instructions'. 
+                    
+                    The 'nutrients' property should have the following sub-properties: 'calories' (IMPORTANT TO GET THIS ONE CORRECT), 'protein', 'carbohydrates', 'fats', 'fiber', 'sugars'. 
+                    
+                    The 'ingredients' property should be an array of objects containing string 'unit', number or decimal 'quantity', string 'name', and a boolean 'optional'. 
+                    
+                    The 'cooktime_in_minutes' property should be a number.
+                    
+                    The 'instructions' property should be an array of objects containing number 'step' and string 'description'.
+                    "
+                ],
+                [
+                    "role" => "user",
+                    "content" => $content
+                ]
+            ],
+            "temperature" => 1.0,
+            "max_tokens" => 4000,
+            "frequency_penalty" => 0,
+            "presence_penalty" => 0,
+        ]);
+        "https://i.imghippo.com/files/6CX4m1722608547.jpg";
     }
 }
