a8e42d4e15501c720f34528372abb4e94e6352dc
laupwing <laupwing@gmail.com>
2024-08-02 14:41:54 +0200

    chore: Add lazy loading for images and improve date display on Welcome page

--- resources/js/Pages/Welcome.tsx
+++ resources/js/Pages/Welcome.tsx
@@ -1,4 +1,6 @@
+import { cn, generateDateArray } from "@/lib/utils"
 import { Head } from "@inertiajs/react"
+import { useEffect, useRef } from "react"
 export default function Welcome() {
     const date = new Date()
     return (
@@ -12,10 +14,59 @@ export default function Welcome() {
                                 month: "long",
                             })}
                         </h2>
-                        {/* <Dates /> */}
+                        <Dates />
                     </div>
                 </header>
             </div>
         </>
+    )
+}
+
+const Dates = () => {
+    const today = new Date()
+    const start_date = new Date()
+    start_date.setDate(today.getDate() - 14)
+    const end_date = new Date()
+    end_date.setDate(today.getDate() + 14)
+    const date_array = generateDateArray(start_date, end_date)
+    const date_container = useRef<HTMLUListElement>(null)
+
+    const DateItem = ({ date }: { date: Date }) => {
+        const date_item = useRef<HTMLLIElement>(null)
+        useEffect(() => {
+            if (date.toLocaleDateString() === today.toLocaleDateString()) {
+                date_container.current?.scrollTo({
+                    left: date_item.current!.offsetLeft,
+                })
+            }
+        }, [])
+        return (
+            <li
+                ref={date_item}
+                className={cn(
+                    "w-[14.3%] snap-start flex flex-col py-2 rounded-xl flex-shrink-0 text-center",
+                    date.toLocaleDateString() === today.toLocaleDateString() &&
+                        "bg-yellow-400"
+                )}
+            >
+                <b className="text-lg">{date.getDate()}</b>
+                <span className="text-xs">
+                    {date.toLocaleString("en-US", {
+                        weekday: "short",
+                    })}
+                </span>
+            </li>
+        )
+    }
+
+    return (
+        <ul
+            ref={date_container}
+            className="flex mt-4 snap-x gap-2 pb-4 w-full overflow-x-auto"
+        >
+            {date_array.map((date) => (
+                <DateItem key={date.toISOString()} date={date} />
+            ))}
+        </ul>
     )
 }
--- resources/js/lib/utils.ts
+++ resources/js/lib/utils.ts
@@ -2,5 +2,17 @@ import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+    return twMerge(clsx(inputs))
+}
+
+export function generateDateArray(start_date: Date, end_date: Date) {
+    const dates = []
+    let current_date = new Date(start_date)
+
+    while (current_date <= end_date) {
+        dates.push(new Date(current_date))
+        current_date.setDate(current_date.getDate() + 1)
+    }
+
+    return dates
 }
