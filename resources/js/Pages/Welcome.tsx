e4f47664c4e3a41bc5f56fa8c1512a51ccb7e7d2
laupwing <laupwing@gmail.com>
2024-08-02 17:08:42 +0200

    Refactor Welcome page layout to use UploadModal component

--- resources/js/Pages/Welcome.tsx
+++ resources/js/Pages/Welcome.tsx
@@ -78,11 +78,7 @@ export default function Welcome() {
                             </div>
                         </Card>
                         <div className="flex flex-col items-center">
-                            <div className="w-[2px] h-14 bg-slate-200"></div>
-                            <button className="w-10 h-10 flex items-center justify-center rounded-full border-slate-200 border shadow">
-                                <Plus className="text-slate-400" />
-                            </button>
-                            <div className="w-[2px] h-14 bg-slate-200"></div>
+                            <div className="w-[2px] h-20 bg-slate-200"></div>
                         </div>
 
                         <Card className="max-w-[300px] mx-auto text-sm w-full">
