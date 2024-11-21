bd600ac772fe69add2bbc1d72f788f0e368fd98d
laupwing <laupwing@gmail.com>
2024-11-21 17:22:12 +0100

    Refactor Welcome page layout for improved accessibility and user experience

--- resources/js/Pages/Welcome.tsx
+++ resources/js/Pages/Welcome.tsx
@@ -12,124 +12,129 @@ import {
     SidebarContent,
     SidebarGroup,
     SidebarHeader,
+    SidebarInset,
     SidebarMenu,
     SidebarMenuButton,
     SidebarMenuItem,
     SidebarMenuSub,
     SidebarMenuSubButton,
     SidebarMenuSubItem,
+    SidebarProvider,
 } from "@/Components/ui/sidebar"
 
 export default function Welcome() {
     const date = new Date()
     return (
-        <>
-            <Head title="Welcome" />
-            <div className="mx-auto bg-background flex flex-col">
-                <header>
-                    <div className="">
-                        <h2 className="px-4 pt-3 pb-2 uppercase text-slate-300 font-bold">
-                            {date.toLocaleString("en-US", {
-                                month: "long",
-                            })}
-                        </h2>
-                        <Dates />
-                    </div>
-                </header>
+        <SidebarProvider>
+            <Head title="Dashboard" />
+            <AppSidebar />
+            <SidebarInset>
+                <div className="bg-background flex flex-col">
+                    <header>
+                        <div className="">
+                            <h2 className="px-4 pt-3 pb-2 uppercase text-slate-300 font-bold">
+                                {date.toLocaleString("en-US", {
+                                    month: "long",
+                                })}
+                            </h2>
+                            <Dates />
+                        </div>
+                    </header>
 
-                <main className="flex flex-col w-full flex-1 min-h-0 mt-8 items-center">
-                    <div className="flex gap-2 w-full px-4 pt-2 pb-4 text-sm">
-                        <div className="px-2 py-1 rounded-lg bg-green-300">
-                            1800 calories
-                        </div>
-                    </div>
-                    <ScrollArea className="flex w-full flex-col justify-center items-center">
-                        <Card className="max-w-[300px] text-sm mx-auto w-full">
-                            <div className="flex flex-col p-6 gap-2">
-                                <header className="flex flex-col">
-                                    <span className="text-slate-400">
-                                        18:15
-                                    </span>
-                                    <h2 className="font-bold text-base">
-                                        Spaghetti Bolgonese
-                                    </h2>
-                                    <div className="flex text-xs flex-wrap text-[10px] mt-2 gap-x-2 gap-y-1">
-                                        <span className="bg-green-300  text-slate-600 font-bold uppercase py-0.5 px-2 rounded-md">
-                                            913 calories
-                                        </span>
-                                        <span className="bg-blue-300 text-slate-600 font-bold uppercase py-0.5 px-2 rounded-md">
-                                            98 protein
-                                        </span>
-                                        <span className="bg-red-300 text-slate-600 font-bold uppercase py-0.5 px-2 rounded-md">
-                                            98 carb
-                                        </span>
-                                        <span className="bg-yellow-300 text-slate-600 font-bold uppercase py-0.5 px-2 rounded-md">
-                                            98 fiber
-                                        </span>
-                                        <span className="bg-orange-300 text-slate-600 font-bold uppercase py-0.5 px-2 rounded-md">
-                                            98 fats
-                                        </span>
-                                    </div>
-                                </header>
-                                <div className="mt-2">
-                                    <img
-                                        className="w-full rounded h-32 object-cover"
-                                        src="https://www.allrecipes.com/thmb/mvO1mRRH1zTz1SvbwBCTz78CRJI=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/67700_RichPastaforthePoorKitchen_ddmfs_4x3_2284-220302ec8328442096df370dede357d7.jpg"
-                                        alt=""
-                                    />
-                                </div>
+                    <main className="flex flex-col w-full flex-1 min-h-0 mt-8 items-center">
+                        <div className="flex gap-2 w-full px-4 pt-2 pb-4 text-sm">
+                            <div className="px-2 py-1 rounded-lg bg-green-300">
+                                1800 calories
                             </div>
-                        </Card>
-                        <div className="flex flex-col items-center">
-                            <div className="w-[2px] h-20 bg-slate-200"></div>
                         </div>
+                        <ScrollArea className="flex w-full flex-col justify-center items-center">
+                            <Card className="max-w-[300px] text-sm mx-auto w-full">
+                                <div className="flex flex-col p-6 gap-2">
+                                    <header className="flex flex-col">
+                                        <span className="text-slate-400">
+                                            18:15
+                                        </span>
+                                        <h2 className="font-bold text-base">
+                                            Spaghetti Bolgonese
+                                        </h2>
+                                        <div className="flex text-xs flex-wrap text-[10px] mt-2 gap-x-2 gap-y-1">
+                                            <span className="bg-green-300  text-slate-600 font-bold uppercase py-0.5 px-2 rounded-md">
+                                                913 calories
+                                            </span>
+                                            <span className="bg-blue-300 text-slate-600 font-bold uppercase py-0.5 px-2 rounded-md">
+                                                98 protein
+                                            </span>
+                                            <span className="bg-red-300 text-slate-600 font-bold uppercase py-0.5 px-2 rounded-md">
+                                                98 carb
+                                            </span>
+                                            <span className="bg-yellow-300 text-slate-600 font-bold uppercase py-0.5 px-2 rounded-md">
+                                                98 fiber
+                                            </span>
+                                            <span className="bg-orange-300 text-slate-600 font-bold uppercase py-0.5 px-2 rounded-md">
+                                                98 fats
+                                            </span>
+                                        </div>
+                                    </header>
+                                    <div className="mt-2">
+                                        <img
+                                            className="w-full rounded h-32 object-cover"
+                                            src="https://www.allrecipes.com/thmb/mvO1mRRH1zTz1SvbwBCTz78CRJI=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/67700_RichPastaforthePoorKitchen_ddmfs_4x3_2284-220302ec8328442096df370dede357d7.jpg"
+                                            alt=""
+                                        />
+                                    </div>
+                                </div>
+                            </Card>
+                            <div className="flex flex-col items-center">
+                                <div className="w-[2px] h-20 bg-slate-200"></div>
+                            </div>
 
-                        <Card className="max-w-[300px] mx-auto text-sm w-full">
-                            <div className="flex flex-col p-6 gap-2">
-                                <header className="flex flex-col">
-                                    <span className="text-slate-400">
-                                        18:15
-                                    </span>
-                                    <h2 className="font-bold text-base">
-                                        Spaghetti Bolgonese
-                                    </h2>
-                                    <div className="flex text-xs flex-wrap text-[10px] mt-2 gap-x-2 gap-y-1">
-                                        <span className="bg-green-300  text-slate-600 font-bold uppercase py-0.5 px-2 rounded-md">
-                                            913 calories
+                            <Card className="max-w-[300px] mx-auto text-sm w-full">
+                                <div className="flex flex-col p-6 gap-2">
+                                    <header className="flex flex-col">
+                                        <span className="text-slate-400">
+                                            18:15
                                         </span>
-                                        <span className="bg-blue-300 text-slate-600 font-bold uppercase py-0.5 px-2 rounded-md">
-                                            98 protein
-                                        </span>
-                                        <span className="bg-red-300 text-slate-600 font-bold uppercase py-0.5 px-2 rounded-md">
-                                            98 carb
-                                        </span>
+                                        <h2 className="font-bold text-base">
+                                            Spaghetti Bolgonese
+                                        </h2>
+                                        <div className="flex text-xs flex-wrap text-[10px] mt-2 gap-x-2 gap-y-1">
+                                            <span className="bg-green-300  text-slate-600 font-bold uppercase py-0.5 px-2 rounded-md">
+                                                913 calories
+                                            </span>
+                                            <span className="bg-blue-300 text-slate-600 font-bold uppercase py-0.5 px-2 rounded-md">
+                                                98 protein
+                                            </span>
+                                            <span className="bg-red-300 text-slate-600 font-bold uppercase py-0.5 px-2 rounded-md">
+                                                98 carb
+                                            </span>
+                                        </div>
+                                    </header>
+                                    <div className="mt-2">
+                                        <img
+                                            className="w-full rounded h-32 object-cover"
+                                            src="https://www.allrecipes.com/thmb/mvO1mRRH1zTz1SvbwBCTz78CRJI=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/67700_RichPastaforthePoorKitchen_ddmfs_4x3_2284-220302ec8328442096df370dede357d7.jpg"
+                                            alt=""
+                                        />
                                     </div>
-                                </header>
-                                <div className="mt-2">
-                                    <img
-                                        className="w-full rounded h-32 object-cover"
-                                        src="https://www.allrecipes.com/thmb/mvO1mRRH1zTz1SvbwBCTz78CRJI=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/67700_RichPastaforthePoorKitchen_ddmfs_4x3_2284-220302ec8328442096df370dede357d7.jpg"
-                                        alt=""
-                                    />
                                 </div>
+                            </Card>
+                            <div className="flex flex-col items-center">
+                                <div className="w-[2px] h-14 bg-slate-200"></div>
+                                <Dialog>
+                                    <DialogTrigger asChild>
+                                        <button className="w-10 h-10 flex items-center justify-center rounded-full border-slate-200 border shadow">
+                                            <Plus className="text-slate-400" />
+                                        </button>
+                                    </DialogTrigger>
+                                    <MealModal />
+                                </Dialog>
                             </div>
-                        </Card>
-                        <div className="flex flex-col items-center">
-                            <div className="w-[2px] h-14 bg-slate-200"></div>
-                            <Dialog>
-                                <DialogTrigger asChild>
-                                    <button className="w-10 h-10 flex items-center justify-center rounded-full border-slate-200 border shadow">
-                                        <Plus className="text-slate-400" />
-                                    </button>
-                                </DialogTrigger>
-                                <MealModal />
-                            </Dialog>
-                        </div>
-                        <div className="h-8"></div>
-                    </ScrollArea>
-                </main>
-            </div>
-        </>
+                            <div className="h-8"></div>
+                        </ScrollArea>
+                    </main>
+                </div>
+            </SidebarInset>
+        </SidebarProvider>
     )
 }
 
