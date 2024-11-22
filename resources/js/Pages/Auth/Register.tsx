cac03cfaede83b5a5aa9142cb3a855cd650c0f38
laupwing <laupwing@gmail.com>
2024-11-22 14:06:48 +0100

    Refactor Register component to use new UI components and add user info step

--- resources/js/Pages/Auth/Register.tsx
+++ resources/js/Pages/Auth/Register.tsx
@@ -1,26 +1,30 @@
-import { FormEventHandler } from 'react';
-import GuestLayout from '@/Layouts/GuestLayout';
-import InputError from '@/Components/InputError';
-import InputLabel from '@/Components/InputLabel';
-import PrimaryButton from '@/Components/PrimaryButton';
-import TextInput from '@/Components/TextInput';
-import { Head, Link, useForm } from '@inertiajs/react';
+import { FormEventHandler } from "react"
+import GuestLayout from "@/Layouts/GuestLayout"
+import InputError from "@/Components/InputError"
+import InputLabel from "@/Components/InputLabel"
+import PrimaryButton from "@/Components/PrimaryButton"
+import TextInput from "@/Components/TextInput"
+import { Head, Link, useForm } from "@inertiajs/react"
+import { Label } from "@/Components/ui/label"
+import { Input } from "@/Components/ui/input"
+import { Button } from "@/Components/ui/button"
+import { ChevronRight } from "lucide-react"
 
 export default function Register() {
     const { data, setData, post, processing, errors, reset } = useForm({
-        name: '',
-        email: '',
-        password: '',
-        password_confirmation: '',
-    });
+        name: "",
+        email: "",
+        password: "",
+        password_confirmation: "",
+    })
 
     const submit: FormEventHandler = (e) => {
-        e.preventDefault();
+        e.preventDefault()
 
-        post(route('register'), {
-            onFinish: () => reset('password', 'password_confirmation'),
-        });
-    };
+        post(route("register"), {
+            onFinish: () => reset("password", "password_confirmation"),
+        })
+    }
 
     return (
         <GuestLayout>
@@ -37,7 +41,7 @@ export default function Register() {
                         className="mt-1 block w-full"
                         autoComplete="name"
                         isFocused={true}
-                        onChange={(e) => setData('name', e.target.value)}
+                        onChange={(e) => setData("name", e.target.value)}
                         required
                     />
 
@@ -54,7 +58,7 @@ export default function Register() {
                         value={data.email}
                         className="mt-1 block w-full"
                         autoComplete="username"
-                        onChange={(e) => setData('email', e.target.value)}
+                        onChange={(e) => setData("email", e.target.value)}
                         required
                     />
 
@@ -71,7 +75,7 @@ export default function Register() {
                         value={data.password}
                         className="mt-1 block w-full"
                         autoComplete="new-password"
-                        onChange={(e) => setData('password', e.target.value)}
+                        onChange={(e) => setData("password", e.target.value)}
                         required
                     />
 
@@ -79,7 +83,10 @@ export default function Register() {
                 </div>
 
                 <div className="mt-4">
-                    <InputLabel htmlFor="password_confirmation" value="Confirm Password" />
+                    <InputLabel
+                        htmlFor="password_confirmation"
+                        value="Confirm Password"
+                    />
 
                     <TextInput
                         id="password_confirmation"
@@ -88,16 +95,21 @@ export default function Register() {
                         value={data.password_confirmation}
                         className="mt-1 block w-full"
                         autoComplete="new-password"
-                        onChange={(e) => setData('password_confirmation', e.target.value)}
+                        onChange={(e) =>
+                            setData("password_confirmation", e.target.value)
+                        }
                         required
                     />
 
-                    <InputError message={errors.password_confirmation} className="mt-2" />
+                    <InputError
+                        message={errors.password_confirmation}
+                        className="mt-2"
+                    />
                 </div>
 
                 <div className="flex items-center justify-end mt-4">
                     <Link
-                        href={route('login')}
+                        href={route("login")}
                         className="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
                     >
                         Already registered?
@@ -109,5 +121,56 @@ export default function Register() {
                 </div>
             </form>
         </GuestLayout>
-    );
+    )
+}
+
+const Step2 = () => {
+    return (
+        <form className="flex flex-col">
+            <div className="text-background my-8 flex flex-col">
+                <h2 className="text-xl font-bold">User Info</h2>
+                <p className="text-sm">
+                    User info is used to personalize your experience for the
+                    fitness app.
+                </p>
+            </div>
+            <div className="grid gap-4">
+                <div className="grid gap-1 text-background">
+                    <Label htmlFor="firstname">Firstname</Label>
+                    <Input id="firstname" type="text" autoCorrect="off" />
+                </div>
+                <div className="grid gap-1 text-background">
+                    <Label htmlFor="lastname">Lastname</Label>
+                    <Input id="lastname" type="text" autoCorrect="off" />
+                </div>
+                <div className="grid gap-1 text-background">
+                    <Label htmlFor="dateOfBirth">Date of Birth</Label>
+                    <Input id="dateOfBirth" type="date" autoCorrect="off" />
+                </div>
+                <div className="grid gap-1 text-background">
+                    <Label htmlFor="dateOfBirth">Height</Label>
+                    <div className="flex gap-1">
+                        <Input
+                            id="dateOfBirth"
+                            type="number"
+                            autoCorrect="off"
+                        />
+                        {/* <Select defaultValue="inch">
+                            <SelectTrigger className="w-[180px]">
+                                <SelectValue placeholder="Height" />
+                            </SelectTrigger>
+                            <SelectContent>
+                                <SelectItem value="cm">CM</SelectItem>
+                                <SelectItem value="inch">INCH</SelectItem>
+                            </SelectContent>
+                        </Select> */}
+                    </div>
+                </div>
+            </div>
+            <Button variant={"secondary"} className="ml-auto mt-4">
+                <span>Next</span>
+                <ChevronRight className="size-6" />
+            </Button>
+        </form>
+    )
 }
