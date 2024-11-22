0df555dd9b567f2737f19e208672e0e1aa3a81f7
laupwing <laupwing@gmail.com>
2024-11-22 14:27:00 +0100

    Add Step 3 to Register component for goal setting with weight input and duration selection

--- resources/js/Pages/Auth/Register.tsx
+++ resources/js/Pages/Auth/Register.tsx
@@ -8,7 +8,7 @@ import { Head, Link, useForm } from "@inertiajs/react"
 import { Label } from "@/Components/ui/label"
 import { Input } from "@/Components/ui/input"
 import { Button } from "@/Components/ui/button"
-import { ChevronRight } from "lucide-react"
+import { ChevronLeft, ChevronRight } from "lucide-react"
 import {
     Select,
     SelectContent,
@@ -18,12 +18,14 @@ import {
 } from "@/Components/ui/select"
 
 export default function Register() {
-    const [currentStep, setCurrentStep] = useState(2)
+    const [currentStep, setCurrentStep] = useState(3)
 
     const renderStep = () => {
         switch (currentStep) {
             case 1:
                 return <Step2 />
+            case 3:
+                return <Step3 />
             default:
                 return <Step2 />
         }
@@ -122,6 +124,67 @@ const Step2 = () => {
                 <span>Next</span>
                 <ChevronRight className="size-6" />
             </Button>
+        </form>
+    )
+}
+
+const Step3 = () => {
+    return (
+        <form className="flex flex-col">
+            <div className=" my-8 flex flex-col">
+                <h2 className="text-xl font-bold">Your Goal</h2>
+                <p className="text-sm">
+                    Your goal is used to personalize your experience for the
+                    fitness app. Keep in mind you can lose 1-2 lbs (0.5 - 1kg)
+                    per week.
+                </p>
+            </div>
+            <div className="grid gap-4">
+                <div className="grid gap-1 ">
+                    <Label htmlFor="dateOfBirth">Ideal Weight</Label>
+                    <div className="flex gap-1">
+                        <Input
+                            id="dateOfBirth"
+                            type="number"
+                            autoCorrect="off"
+                        />
+                        <Select defaultValue="lbs">
+                            <SelectTrigger className="w-[180px]">
+                                <SelectValue placeholder="weight" />
+                            </SelectTrigger>
+                            <SelectContent>
+                                <SelectItem value="kg">KG</SelectItem>
+                                <SelectItem value="lbs">LBS</SelectItem>
+                            </SelectContent>
+                        </Select>
+                    </div>
+                </div>
+                <div className="grid gap-1 ">
+                    <Label htmlFor="dateOfBirth">Months to hit the goal</Label>
+                    <Select defaultValue="inch">
+                        <SelectTrigger className="w-auto">
+                            <SelectValue placeholder="Gender" />
+                        </SelectTrigger>
+                        <SelectContent>
+                            {new Array(12).fill(0).map((_, i) => (
+                                <SelectItem value={(i + 1).toString()}>
+                                    {i + 1}
+                                </SelectItem>
+                            ))}
+                        </SelectContent>
+                    </Select>
+                </div>
+            </div>
+            <div className="mt-4 flex justify-between">
+                <Button variant={"outline"}>
+                    <span>Prev</span>
+                    <ChevronLeft className="size-6" />
+                </Button>
+                <Button className="">
+                    <span>Next</span>
+                    <ChevronRight className="size-6" />
+                </Button>
+            </div>
         </form>
     )
 }
