0da57b530c6a77c78d31c9127a681f28579f675a
laupwing <laupwing@gmail.com>
2024-11-22 14:12:06 +0100

    Refactor Register component to implement multi-step user registration and integrate new UI elements

--- resources/js/Pages/Auth/Register.tsx
+++ resources/js/Pages/Auth/Register.tsx
@@ -1,4 +1,4 @@
-import { FormEventHandler } from "react"
+import { FormEventHandler, useState } from "react"
 import GuestLayout from "@/Layouts/GuestLayout"
 import InputError from "@/Components/InputError"
 import InputLabel from "@/Components/InputLabel"
@@ -9,125 +9,37 @@ import { Label } from "@/Components/ui/label"
 import { Input } from "@/Components/ui/input"
 import { Button } from "@/Components/ui/button"
 import { ChevronRight } from "lucide-react"
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/Components/ui/select"
 
 export default function Register() {
-    const { data, setData, post, processing, errors, reset } = useForm({
-        name: "",
-        email: "",
-        password: "",
-        password_confirmation: "",
-    })
+    const [currentStep, setCurrentStep] = useState(2)
 
-    const submit: FormEventHandler = (e) => {
-        e.preventDefault()
-
-        post(route("register"), {
-            onFinish: () => reset("password", "password_confirmation"),
-        })
+    const renderStep = () => {
+        switch (currentStep) {
+            case 1:
+                return <Step2 />
+            default:
+                return <Step2 />
+        }
     }
 
     return (
-        <GuestLayout>
-            <Head title="Register" />
-
-            <form onSubmit={submit}>
-                <div>
-                    <InputLabel htmlFor="name" value="Name" />
-
-                    <TextInput
-                        id="name"
-                        name="name"
-                        value={data.name}
-                        className="mt-1 block w-full"
-                        autoComplete="name"
-                        isFocused={true}
-                        onChange={(e) => setData("name", e.target.value)}
-                        required
-                    />
-
-                    <InputError message={errors.name} className="mt-2" />
-                </div>
-
-                <div className="mt-4">
-                    <InputLabel htmlFor="email" value="Email" />
-
-                    <TextInput
-                        id="email"
-                        type="email"
-                        name="email"
-                        value={data.email}
-                        className="mt-1 block w-full"
-                        autoComplete="username"
-                        onChange={(e) => setData("email", e.target.value)}
-                        required
-                    />
-
-                    <InputError message={errors.email} className="mt-2" />
-                </div>
-
-                <div className="mt-4">
-                    <InputLabel htmlFor="password" value="Password" />
-
-                    <TextInput
-                        id="password"
-                        type="password"
-                        name="password"
-                        value={data.password}
-                        className="mt-1 block w-full"
-                        autoComplete="new-password"
-                        onChange={(e) => setData("password", e.target.value)}
-                        required
-                    />
-
-                    <InputError message={errors.password} className="mt-2" />
-                </div>
-
-                <div className="mt-4">
-                    <InputLabel
-                        htmlFor="password_confirmation"
-                        value="Confirm Password"
-                    />
-
-                    <TextInput
-                        id="password_confirmation"
-                        type="password"
-                        name="password_confirmation"
-                        value={data.password_confirmation}
-                        className="mt-1 block w-full"
-                        autoComplete="new-password"
-                        onChange={(e) =>
-                            setData("password_confirmation", e.target.value)
-                        }
-                        required
-                    />
-
-                    <InputError
-                        message={errors.password_confirmation}
-                        className="mt-2"
-                    />
-                </div>
-
-                <div className="flex items-center justify-end mt-4">
-                    <Link
-                        href={route("login")}
-                        className="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
-                    >
-                        Already registered?
-                    </Link>
-
-                    <PrimaryButton className="ms-4" disabled={processing}>
-                        Register
-                    </PrimaryButton>
-                </div>
-            </form>
-        </GuestLayout>
+        <div className="text-black">
+            <div>{renderStep()}</div>
+        </div>
     )
 }
 
 const Step2 = () => {
     return (
         <form className="flex flex-col">
-            <div className="text-background my-8 flex flex-col">
+            <div className=" my-8 flex flex-col">
                 <h2 className="text-xl font-bold">User Info</h2>
                 <p className="text-sm">
                     User info is used to personalize your experience for the
@@ -135,19 +47,19 @@ const Step2 = () => {
                 </p>
             </div>
             <div className="grid gap-4">
-                <div className="grid gap-1 text-background">
+                <div className="grid gap-1 ">
                     <Label htmlFor="firstname">Firstname</Label>
                     <Input id="firstname" type="text" autoCorrect="off" />
                 </div>
-                <div className="grid gap-1 text-background">
+                <div className="grid gap-1 ">
                     <Label htmlFor="lastname">Lastname</Label>
                     <Input id="lastname" type="text" autoCorrect="off" />
                 </div>
-                <div className="grid gap-1 text-background">
+                <div className="grid gap-1 ">
                     <Label htmlFor="dateOfBirth">Date of Birth</Label>
                     <Input id="dateOfBirth" type="date" autoCorrect="off" />
                 </div>
-                <div className="grid gap-1 text-background">
+                <div className="grid gap-1 ">
                     <Label htmlFor="dateOfBirth">Height</Label>
                     <div className="flex gap-1">
                         <Input
@@ -155,7 +67,7 @@ const Step2 = () => {
                             type="number"
                             autoCorrect="off"
                         />
-                        {/* <Select defaultValue="inch">
+                        <Select defaultValue="inch">
                             <SelectTrigger className="w-[180px]">
                                 <SelectValue placeholder="Height" />
                             </SelectTrigger>
@@ -163,11 +75,11 @@ const Step2 = () => {
                                 <SelectItem value="cm">CM</SelectItem>
                                 <SelectItem value="inch">INCH</SelectItem>
                             </SelectContent>
-                        </Select> */}
+                        </Select>
                     </div>
                 </div>
             </div>
-            <Button variant={"secondary"} className="ml-auto mt-4">
+            <Button className="ml-auto mt-4">
                 <span>Next</span>
                 <ChevronRight className="size-6" />
             </Button>
