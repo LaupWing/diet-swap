b4c908015284ab882a4346b1e5caafe5e845fced
laupwing <laupwing@gmail.com>
2024-11-23 17:09:10 +0100

    Implement meal analysis logic in MealController with validation from AnalyzeMealRequest

--- app/Http/Controllers/MealController.php
+++ app/Http/Controllers/MealController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\AnalyzeMealRequest;
 use Illuminate\Http\Request;
 
 class MealController extends Controller
 {
-    public function analyze() {}
+    public function analyze(AnalyzeMealRequest $request)
+    {
+        $data = $request->validated();
+
+        $picture = $data["picture"];
+        $name = $data["name"];
+        $description = $data["description"];
+
+        $base64Image = base64_encode(file_get_contents($picture->getRealPath()));
+    }
 }
