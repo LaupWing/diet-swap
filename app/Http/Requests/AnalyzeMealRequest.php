0b8c23c4ad5b3edb57d535a1de5bf83b6db43b4d
laupwing <laupwing@gmail.com>
2024-11-23 17:06:55 +0100

    Update AnalyzeMealRequest to authorize all requests and add validation rules for picture, name, and description

--- app/Http/Requests/AnalyzeMealRequest.php
+++ app/Http/Requests/AnalyzeMealRequest.php
@@ -11,7 +11,7 @@ class AnalyzeMealRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,9 @@ class AnalyzeMealRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'picture' => ['required', 'file', 'mimes:jpg,jpeg,png,gif', 'max:20480'],
+            'name' => ['required', 'string'],
+            'description' => ['required', 'string'],
         ];
     }
 }
