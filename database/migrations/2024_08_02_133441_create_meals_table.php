290c87011295abef7fc6e68d2cbea2abcb86208e
laupwing <laupwing@gmail.com>
2024-11-23 13:36:24 +0100

    Add name field to meals table migration

--- database/migrations/2024_08_02_133441_create_meals_table.php
+++ database/migrations/2024_08_02_133441_create_meals_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('meals', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('name');
             $table->text('description');
             $table->string('calories');
             $table->string('protein');
