2f15b6c7618cb4d590b9b65a8860c356071eda96
laupwing <laupwing@gmail.com>
2024-11-23 13:30:44 +0100

    updated

--- app/Models/UserInfo.php
+++ app/Models/UserInfo.php
@@ -4,8 +4,33 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class UserInfo extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'firstname',
+        'lastname',
+        'weight',
+        'height',
+        'gender',
+        'date_of_birth',
+        'ideal_weight',
+        'ideal_weight_timespan_in_months',
+        'weight_unit',
+        'height_unit',
+        'activity_level',
+        'allergies',
+        'cuisine',
+        'dietary',
+        'special_notes',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
