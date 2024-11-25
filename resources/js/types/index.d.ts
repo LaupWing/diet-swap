caae39815c412a880acdab7e49033007afb1aba2
laupwing <laupwing@gmail.com>
2024-11-25 17:35:31 +0100

    added all the types

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -1,12 +1,30 @@
 export interface User {
-    id: number;
-    name: string;
-    email: string;
-    email_verified_at: string;
+    id: number
+    name: string
+    email: string
+    email_verified_at: string
 }
 
-export type PageProps<T extends Record<string, unknown> = Record<string, unknown>> = T & {
+export type PageProps<
+    T extends Record<string, unknown> = Record<string, unknown>
+> = T & {
     auth: {
-        user: User;
-    };
-};
+        user: User
+    }
+}
+
+interface DataRecord {
+    created_at: string
+    id: number
+    updated_at: string
+}
+
+export interface UserGoal extends DataRecord {
+    id: number
+    user_id: number
+    calories: string
+    protein: string
+    current_bodyfat: string
+    goal_bodyfat: string
+    meal_plan: string
+}
