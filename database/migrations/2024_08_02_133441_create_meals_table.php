4a1cd46b119f4f6cd2d00c8666259966e6132cae
laupwing <laupwing@gmail.com>
2024-11-26 16:10:58 +0100

    Replace user_id with picture_id foreign key in meals table migration

--- database/migrations/2024_08_02_133441_create_meals_table.php
+++ database/migrations/2024_08_02_133441_create_meals_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('meals', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('picture_id')->constrained()->onDelete('cascade');
             $table->string('name');
             $table->text('description');
             $table->string('is_healthy');
