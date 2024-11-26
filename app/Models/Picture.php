7dc573d7cb4c4a4cf246f78eb5ec12f927ec0f72
laupwing <laupwing@gmail.com>
2024-11-26 15:49:51 +0100

    Add methods to generate S3 file path and URL in Picture model

--- app/Models/Picture.php
+++ app/Models/Picture.php
@@ -4,8 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Picture extends Model
 {
     use HasFactory;
+    public function getFilePathAttribute(): string
+    {
+        return "/{$this->threadItem->thread->user_id}/{$this->threadItem->id}/{$this->id}.{$this->file_extension}";
+    }
+
+    public function getFileFromS3(): string
+    {
+        /** @var \Illuminate\Filesystem\FilesystemManager $disk */
+        $disk = Storage::disk('pictures');
+        return $disk->url($this->file_path);
+    }
+
+    public function getS3UrlAttribute()
+    {
+        return $this->getFileFromS3();
+    }
 }
