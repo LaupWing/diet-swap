96d7552272f80214526f86dab05818bc8a59b546
laupwing <laupwing@gmail.com>
2024-11-26 15:48:48 +0100

    Add is_healthy field to meals table migration

--- database/migrations/2024_08_02_133441_create_meals_table.php
+++ database/migrations/2024_08_02_133441_create_meals_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('name');
             $table->text('description');
+            $table->string('is_healthy');
             $table->string('calories');
             $table->string('protein');
             $table->string('carbs');
