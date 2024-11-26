8ec2cc146362f95c2f3aaf7f5ceab8c557c9b0f2
laupwing <laupwing@gmail.com>
2024-11-26 19:36:56 +0100

    Refactor MealCard component to include meal details in a dialog and replace inline meal display in Welcome page with MealCard component

--- resources/js/Components/MealCard.tsx
+++ resources/js/Components/MealCard.tsx
@@ -1,8 +1,17 @@
-import { Picture } from "@/types"
-import React, { FC } from "react"
+import { Meal, Picture } from "@/types"
+import { FC } from "react"
 import { Card } from "./ui/card"
 import { Info } from "lucide-react"
 import { IsHealthy } from "./IsHealthy"
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "./ui/dialog"
+import { Button } from "./ui/button"
 
 export const MealCard: FC<{
     picture: Picture
@@ -22,9 +31,14 @@ export const MealCard: FC<{
                                 }
                             )}
                         </span>
-                        <button>
-                            <Info />
-                        </button>
+                        <Dialog>
+                            <DialogTrigger asChild>
+                                <button>
+                                    <Info />
+                                </button>
+                            </DialogTrigger>
+                            <MealInfo meal={picture.meal} />
+                        </Dialog>
                     </div>
                     <h2 className="font-bold text-base">{picture.meal.name}</h2>
                     <div className="flex text-xs flex-wrap text-[10px] mt-2 gap-x-2 gap-y-1">
@@ -58,5 +72,54 @@ export const MealCard: FC<{
                 </div>
             </div>
         </Card>
+    )
+}
+
+const MealInfo: FC<{
+    meal: Meal
+}> = ({ meal }) => {
+    return (
+        <DialogContent className="max-w-md w-[90%] rounded">
+            <DialogHeader className="text-left">
+                <DialogTitle>{meal.name}</DialogTitle>
+                <DialogDescription>{meal.description}</DialogDescription>
+            </DialogHeader>
+
+            <div className="flex items-start px-4 gap-3 flex-col">
+                <span className="bg-green-300  text-slate-600 font-bold uppercase py-0.5 px-2 rounded-md">
+                    {meal.calories} calories
+                </span>
+                <span className="bg-blue-300 text-slate-600 font-bold uppercase py-0.5 px-2 rounded-md">
+                    {meal.protein} protein
+                </span>
+                <span className="bg-red-300 text-slate-600 font-bold uppercase py-0.5 px-2 rounded-md">
+                    {meal.carbs} carb
+                </span>
+                <span className="bg-yellow-300 text-slate-600 font-bold uppercase py-0.5 px-2 rounded-md">
+                    {meal.fiber} fiber
+                </span>
+                <span className="bg-orange-300 text-slate-600 font-bold uppercase py-0.5 px-2 rounded-md">
+                    {meal.fats} fats
+                </span>
+                <div className="flex flex-col text-sm gap-2 px-4 py-2 bg-slate-100 rounded">
+                    <div className="flex items-center gap-1">
+                        <span className="uppercase text-slate-500">
+                            Healthy:
+                        </span>{" "}
+                        <IsHealthy healthy={meal.is_healthy!} />
+                    </div>
+                    <p>
+                        <span className="uppercase text-slate-500">
+                            Reason:
+                        </span>{" "}
+                        {meal.is_healthy_reason}
+                    </p>
+                </div>
+                <div className="flex mt-4 justify-between w-full">
+                    <Button>Swap</Button>
+                    <Button variant={"outline"}>Close</Button>
+                </div>
+            </div>
+        </DialogContent>
     )
 }
