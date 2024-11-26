05998e3c9648c545061a5bc9e8141eefb942b9a2
laupwing <laupwing@gmail.com>
2024-11-26 15:56:35 +0100

    Add user relationship to Picture model

--- app/Models/Picture.php
+++ app/Models/Picture.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Support\Facades\Storage;
 
 class Picture extends Model
@@ -30,5 +31,10 @@ class Picture extends Model
     public function getS3UrlAttribute()
     {
         return $this->getFileFromS3();
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
     }
 }
