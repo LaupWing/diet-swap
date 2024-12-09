688f53519f74f56ed1eccf029fc3e6f7e45fb26e
laupwing <laupwing@gmail.com>
2024-12-09 15:40:33 +0100

    Add type field to pictures table migration

--- database/migrations/2024_07_23_163920_create_pictures_table.php
+++ database/migrations/2024_07_23_163920_create_pictures_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('file_extension');
             $table->string('name');
+            $table->string('type');
             $table->string('description');
             $table->timestamps();
         });
