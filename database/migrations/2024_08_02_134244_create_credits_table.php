3fd62653ad612cb27f3015bbbac416008171fffb
laupwing <laupwing@gmail.com>
2024-12-09 18:42:21 +0100

    Set default value for amount field in credits table migration

--- database/migrations/2024_08_02_134244_create_credits_table.php
+++ database/migrations/2024_08_02_134244_create_credits_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('credits', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->integer('amount');
+            $table->integer('amount')->default(10);
             $table->timestamps();
         });
     }
